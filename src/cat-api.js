14bd7b13e344f143c8144f503a01b1ee3eefd15c
alfred161 <149138275+alfred161@users.noreply.github.com>
2024-05-07 23:01:12 +0800

    axios, fixed package.json, and default css

---
+++ src/cat-api.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_3QdbBGoB9DsmMqy9memhYHCvcCxLkbMXp7OsEhRbi11estAuihRtnNRQmeiQjBwb';
+
+function fetchBreeds(params) {}
+
+function fetchCatByBreed(breedId) {}
